5d661a6e6eb0c3535f6c1a1185cddfdddf608f46
sbalaji-code <bs.gcp.free@gmail.com>
2026-03-10 06:34:25 +0000

    second

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "project_id" {
+  type        = string
+  description = "Your Google Cloud project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "The region where BigQuery resources will be created"
+  default = "US" #  A reasonable default, but should be adjusted
+}
+
+variable "dataset_id" {
+  type        = string
+  description = "The ID of the BigQuery dataset to create"
+}
+
+variable "location" {
+  type        = string
+  description = "The geographic location for the dataset"
+  default = "US"
+}
+
+variable "description" {
+  type        = string
+  description = "A description for the dataset"
+  default = "My Terraform-created dataset"
+}
+
+variable "environment" {
+  type = string
+  description = "Environment label for the dataset"
+  default = "dev"
+}
