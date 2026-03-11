f8a60513ff054d9004c1d98ef2943d791dd17682
sbalaji-code <bs.gcp.free@gmail.com>
2026-03-11 03:17:38 +0000

    table create

--- main.tf
+++ main.tf
@@ -15,19 +15,19 @@ resource "google_bigquery_dataset" "new_dataset" {
 }
 
 # Optional:  Create a table within the dataset (example)
-# resource "google_bigquery_table" "example_table" {
-#   dataset_id = google_bigquery_dataset.new_dataset.dataset_id
-#   table_id = "my_example_table"
-#   schema = jsonencode([
-#     {
-#       name = "id"
-#       type = "INT64"
-#       mode = "REQUIRED"
-#     },
-#     {
-#       name = "name"
-#       type = "STRING"
-#       mode = "NULLABLE"
-#     }
-#   ])
-# }
+resource "google_bigquery_table" "example_table" {
+  dataset_id = google_bigquery_dataset.new_dataset.dataset_id
+  table_id = "my_example_table"
+  schema = jsonencode([
+    {
+      name = "id"
+      type = "INT64"
+      mode = "REQUIRED"
+    },
+    {
+      name = "name"
+      type = "STRING"
+      mode = "NULLABLE"
+    }
+  ])
+}
